19a8629fd2e354ff30897d1e119fdf8617a95458
Lucas deOliveira <ludeoliv@ie.ibm.com>
2016-12-15 15:57:35 +0000

    resolving issue #1 and creating a component to connect database

--- src/dbsync/dbsync.js
+++ src/dbsync/dbsync.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import firebase from 'firebase';
+import Firebase from 'firebase';
 
 var config = {
-  apiKey: "AIzaSyDbvEQXI1rv1-AEutNKreqxmWpgjltCh_w",
-  authDomain: "namegenenrator.firebaseapp.com",
-  databaseURL: "https://namegenenrator.firebaseio.com",
-  storageBucket: "namegenenrator.appspot.com",
-  messagingSenderId: "807735289237"
-};
-var app = firebase.initializeApp(config);
+    apiKey: "YOURAPIKEY",
+    authDomain: "namegenenrator.firebaseapp.com",
+    databaseURL: "https://namegenenrator.firebaseio.com",
+    storageBucket: "namegenenrator.appspot.com",
+    messagingSenderId: "YOURSENDER"
+  };
+Firebase.initializeApp(config);
 
 
 
