16ea651ee3f07590299556cd21c9dc9886825ef2
Lucas deOliveira <ludeoliv@ie.ibm.com>
2016-12-15 10:06:25 +0000

    change result component to show a Dialog

--- src/App.js
+++ src/App.js
@@ -2,32 +2,34 @@ import React, { Component } from 'react';
 import logo from './gear.svg';
 import './App.css';
 import Results from './components/result.js';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import RaisedButton from 'material-ui/RaisedButton';
 
 
 var App = React.createClass ({
-
-  getInitialState () {
+/*  getInitialState () {
          return { showResults: false };
    },
    onClick () {
        this.setState({ showResults: true });
-   },
+   },*/
 
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Bem vindo ao Odebrecht : Gerador de Apelidos</h2>
+      <MuiThemeProvider>
+        <div className="App">
+          <div className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h2>Bem vindo ao Odebrecht : Gerador de Apelidos</h2>
+          </div>
+          <p className="App-intro">
+            Para comecar, clique
+          </p>
+          <div>
+            <Results/>
+          </div>
         </div>
-        <p className="App-intro">
-          Para comecar, clique
-        </p>
-        <div>
-          <button onClick={this.onClick}>AQUI</button>
-          <p>{ this.state.showResults ? <Results /> : null }</p>
-        </div>
-      </div>
+      </MuiThemeProvider>
     );
   }
 })
--- src/components/result.js
+++ src/components/result.js
@@ -1,50 +1,80 @@
 import React, { Component } from 'react';
+import Dialog from 'material-ui/Dialog';
+import FlatButton from 'material-ui/FlatButton';
+import RaisedButton from 'material-ui/RaisedButton';
 
 
-var Results = React.createClass ({
-  render () {
-            var names = { "results" :[
-              "porco-velho",
-              "muricoca",
-              "pouco feio",
-              "louca",
-              "Caju",
-              "Justica",
-              "Indio",
-              "Primo",
-              "Las Vegas",
-              "Angora",
-              "Caranguejo",
-              "Cerrado/Piqui",
-              "Polo",
-              "Gremista",
-              "Babel",
-              "Bitelo",
-              "Campari",
-              "Gripado",
-              "Botafogo",
-              "Misericordia",
-              "Ferrari",
-              "Corredor",
-              "Todo Feio",
-              "Jovem",
-              "Feia",
-              "Comuna",
-              "Goleiro",
-              "Diplomata",
-              "Moleza",
-              "Velhinho"
-              ]
-            };
+export default class Results extends React.Component {
+  state = {
+    open: false,
+  };
 
-            var pickresults = names.results;
-            var possible = pickresults[Math.floor(Math.random() * pickresults.length)];
-            return (
-                       <div id="results" className="search-results">{possible}</div>
-                   );
-        }
-})
+  handleOpen = () => {
+    this.setState({open: true});
+  };
 
+  handleClose = () => {
+    this.setState({open: false});
+  };
 
+  render() {
+    var names = { "results" :[
+      "Porco-velho",
+      "Muricoca",
+      "Pouco feio",
+      "Louca",
+      "Caju",
+      "Justica",
+      "Indio",
+      "Primo",
+      "Las Vegas",
+      "Angora",
+      "Caranguejo",
+      "Cerrado/Piqui",
+      "Polo",
+      "Gremista",
+      "Babel",
+      "Bitelo",
+      "Campari",
+      "Gripado",
+      "Botafogo",
+      "Misericordia",
+      "Ferrari",
+      "Corredor",
+      "Todo Feio",
+      "Jovem",
+      "Feia",
+      "Comuna",
+      "Goleiro",
+      "Diplomata",
+      "Moleza",
+      "Velhinho"
+      ]
+    };
 
-export default Results;
+    var pickresults = names.results;
+    var possible = pickresults[Math.floor(Math.random() * pickresults.length)];
+    
+    const actions = [
+      <FlatButton
+        label="OK"
+        primary={true}
+        onClick={this.handleClose}
+      />,
+    ];
+
+    return (
+      <div>
+        <RaisedButton label="Aqui" onClick={this.handleOpen} />
+        <Dialog
+          actions={actions}
+          modal={false}
+          open={this.state.open}
+          onRequestClose={this.handleClose}
+        >
+          {possible}
+        </Dialog>
+      </div>
+    );
+  }
+}
