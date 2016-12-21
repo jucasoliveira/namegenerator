90e48b81eb0da4e2fb44f29b061e0173944287cd
Lucas deOliveira <ludeoliv@ie.ibm.com>
2016-12-21 08:43:29 +0000

    new component changes

--- src/components/dbsync.js
+++ src/components/dbsync.js
@@ -1,23 +1,26 @@
 import React, { Component } from 'react';
 import Firebase from 'firebase';
 
-
+var config = {
+    apiKey: "YOURAPIKEY",
+    authDomain: "namegenenrator.firebaseapp.com",
+    databaseURL: "https://namegenenrator.firebaseio.com",
+    storageBucket: "namegenenrator.appspot.com",
+    messagingSenderId: "YOURSENDER"
+  };
+Firebase.initializeApp(config);
 
 
 
 export default class Dbsync extends React.Component {
-  render() {
-  var config = {
-      apiKey: "YOURAPIKEY",
-      authDomain: "namegenenrator.firebaseapp.com",
-      databaseURL: "https://namegenenrator.firebaseio.com",
-      storageBucket: "namegenenrator.appspot.com",
-      messagingSenderId: "YOURSENDER"
-    };
-  Firebase.initializeApp(config);
-  // Get a reference to the database service
-  var database = Firebase.database();
-  var UCRef = database.ref("names/results");
 
+  database = () => {
+    // Get a reference to the database service
+    var database = Firebase.database();
+    var UCRef = database.ref("names/results");
+
+    return UCRef;
   }
+
+
 }
